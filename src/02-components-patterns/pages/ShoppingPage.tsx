30bf076b78ae8b8fda7698efc07ce84b3ffc141b
Fausto Calviño <faustocalvino@outlook.com>
2023-12-04 12:03:35 +0100

    starting component-control-props branch

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -34,21 +34,7 @@ export const ShoppingPage = () => {
                 <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
                 <ProductTitle className="text-bold" />
                 <ProductButtons className="custom-buttons" />
-            </ProductCard>
-
-            <ProductCard 
-                product={ product }
-                style={{
-                    backgroundColor: '#70D1F8'
-                }}
-            >
-                <ProductImage style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                <ProductTitle style={{ fontWeight: 'bold' }} />
-                <ProductButtons style={{
-                    display: 'flex',
-                    justifyContent: 'end'
-                }}/>
-            </ProductCard>
+            </ProductCard>          
 
         </div>     
     </div>
