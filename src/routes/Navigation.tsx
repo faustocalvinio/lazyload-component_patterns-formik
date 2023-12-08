9aab70a154a7c4fde68c1dd4f75ce0dd9e346c45
Fausto Calviño <faustocalvino@outlook.com>
2023-12-08 20:16:10 +0100

    branch forms initialized

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
 import logo from '../logo.svg'
-import { ShoppingPage } from '../02-components-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
     return (
@@ -12,7 +11,7 @@ export const Navigation = () => {
                     <img src={ logo } alt="React Logo" />
                     <ul>
                         <li>
-                            <NavLink to="/" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Shopping</NavLink>
+                            <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
@@ -27,7 +26,7 @@ export const Navigation = () => {
                 <Routes>
                     <Route path="about" element={ <h1>About Page</h1> } />
                     <Route path="users" element={ <h1>Users Page</h1> } />
-                    <Route path="/" element={ <ShoppingPage /> } />
+                    <Route path="home" element={ <h1>Home Page</h1> } />
                     
                     <Route path="/*" element={ <Navigate to="/home" replace /> } />
                 </Routes>
